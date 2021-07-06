bc1268c3dbd0cd830fe39268afcd1e670c1b26c5
seiyria <kyle@seiyria.com>
2021-07-06 18:49:42 -0500

    remove weaponData

--- src/routes/fight-add.js
+++ src/routes/fight-add.js
@@ -5,7 +5,7 @@ exports.route = (app) => {
   app.get('/leaderboard/fight/add', async (req, res) => {
 
     const { hash, accountAddress, characterId, characterLevel, weaponId, enemyId, wonFight, enemyRoll, playerRoll, xpGain, skillGain } = req.query;
-    if(!hash || !accountAddress || !characterId || !characterLevel || !weaponId || !weaponDAta || !enemyId || !wonFight || !enemyRoll || !playerRoll || !xpGain || !skillGain) {
+    if(!hash || !accountAddress || !characterId || !characterLevel || !weaponId || !enemyId || !wonFight || !enemyRoll || !playerRoll || !xpGain || !skillGain) {
       return res.status(400).json({ error: 'Invalid query. Must pass hash, accountAddress, characterId, characterLevel, weaponId, weaponData, enemyId, wonFight, enemyRoll, playerRoll, xpGain, skillGain.' });
     }
 
