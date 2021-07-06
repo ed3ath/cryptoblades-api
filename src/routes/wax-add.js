fa7d7b414c2cece78c63900aebcff0669f1d410c
seiyria <kyle@seiyria.com>
2021-07-06 12:54:41 -0500

    bscwallet optional

--- src/routes/wax-add.js
+++ src/routes/wax-add.js
@@ -5,7 +5,7 @@ exports.route = (app) => {
   app.get('/wax/add', async (req, res) => {
 
     const { waxWallet, bscWallet, waxAmount, waxChainTransactionId, waxChainBlockNumber, bscTransactionId } = req.query;
-    if(!waxWallet || !bscWallet || !waxAmount || !waxChainTransactionId || !waxChainBlockNumber || !bscTransactionId) {
+    if(!waxWallet || !waxAmount || !waxChainTransactionId || !waxChainBlockNumber || !bscTransactionId) {
       return res.status(400).json({ error: 'Invalid query. Must pass waxWallet, waxAmount, waxChainTransactionId, waxChainBlockNumber, bscTransactionId.' });
     }
 
