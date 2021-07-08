7cd7f13f0a0241e07c89c539d406521b144dea9d
seiyria <kyle@seiyria.com>
2021-07-08 12:20:53 -0500

    add fight loss leaderboard

--- src/tasks/update-leaderboard.js
+++ src/tasks/update-leaderboard.js
@@ -22,12 +22,37 @@ const getFightWinLeaderboard = async () => {
   }));
 };
 
+const getFightLossLeaderboard = async () => {
+  const fightLossLeaderboard = await DB.$fights.aggregate([
+    { $match: { wonFight: 'false' } },
+
+    { $group: { _id: '$characterId', count: { $sum: 1 } } },
+
+    { $sort: { count: -1 } },
+
+    { $limit: 10 }
+  ]).toArray();
+
+  return fightLossLeaderboard.map(x => ({
+    name: `${getCharacterNameFromSeed(x._id)} (ID ${x._id})`,
+    value: x.count
+  }));
+};
+
 exports.task = async () => {
   const fightWins = await getFightWinLeaderboard();
 
   await DB.$leaderboard.replaceOne(
     { key: 'Fights Won' }, 
     { key: 'Fights Won', units: 'Won', leaderboard: fightWins }, 
+    { upsert: true }
+  );
+
+  const fightLosses = await getFightLossLeaderboard();
+
+  await DB.$leaderboard.replaceOne(
+    { key: 'Fights Lost' }, 
+    { key: 'Fights Lost', units: 'Lost', leaderboard: fightLosses }, 
     { upsert: true }
   );
 };
