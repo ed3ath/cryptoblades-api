ec60a830f6552977a526ab44e3689a311537d1ca
seiyria <kyle@seiyria.com>
2021-07-12 11:17:48 -0500

    add failure reason to wax

--- src/routes/wax-add.js
+++ src/routes/wax-add.js
@@ -4,13 +4,13 @@ const { DB } = require('../db');
 exports.route = (app) => {
   app.get('/wax/add', async (req, res) => {
 
-    const { waxWallet, bscWallet, waxAmount, waxChainTransactionId, waxChainBlockNumber, bscTransactionId, gas } = req.query;
+    const { waxWallet, bscWallet, waxAmount, waxChainTransactionId, waxChainBlockNumber, bscTransactionId, failureReason, gas } = req.query;
     if(!waxWallet || !waxAmount || !waxChainTransactionId || !waxChainBlockNumber || !bscTransactionId) {
       return res.status(400).json({ error: 'Invalid query. Must pass waxWallet, waxAmount, waxChainTransactionId, waxChainBlockNumber, bscTransactionId.' });
     }
 
     try {
-      await DB.$log.insertOne({ waxWallet, bscWallet, waxAmount, waxChainTransactionId, waxChainBlockNumber, bscTransactionId, gas });
+      await DB.$log.insertOne({ waxWallet, bscWallet, waxAmount, waxChainTransactionId, waxChainBlockNumber, bscTransactionId, failureReason, gas });
     } catch(error) {
       return res.status(500).json({ error })
     }
