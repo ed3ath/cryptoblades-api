1861983d11e7212560171dc9ad208618679cd0f8
Kyle J. Kemp <kyle@seiyria.com>
2021-07-18 19:19:35 +0000

    try to fix api for market/char

--- src/routes/market-weapon.js
+++ src/routes/market-weapon.js
@@ -104,7 +104,10 @@ exports.route = (app) => {
 
     try {
       const currentMarketEntry = await DB.$marketWeapons.findOne({ weaponId });
-      await DB.$marketSales.insert({ type: 'weapon', ...currentMarketEntry });
+      if(currentMarketEntry) {
+        await DB.$marketSales.insert({ type: 'weapon', ...currentMarketEntry });
+      }
+        
     } catch(error) {
       console.error(error);
       return res.status(500).json({ error })
