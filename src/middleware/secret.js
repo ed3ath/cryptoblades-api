d1e29484780305acbbdf331d9861c694675813f1
seiyria <kyle@seiyria.com>
2021-07-18 12:17:13 -0500

    fix waxapi refs

--- src/middleware/secret.js
+++ src/middleware/secret.js
@@ -4,7 +4,7 @@ module.exports.secretCheck = (req, res, next) => {
   const secret = process.env.API_SECRET;
 
   if(secret && req.query.secret !== secret) {
-    return res.status(403).send({ error: 'Invalid secret. See cryptoblades/cryptoblades-waxapi documentation for how this works.' });
+    return res.status(403).send({ error: 'Invalid secret. See cryptoblades/cryptoblades-api documentation for how this works.' });
   }
 
   next();
