7524c8de2ebdcb06d4ccc67ef762ab6489811728
seiyria <kyle@seiyria.com>
2021-07-21 08:24:07 -0500

    add notifications task, route

---
+++ src/routes/notifications.js
@@ -0,0 +1,10 @@
+
+const { DB } = require('../db');
+
+exports.route = (app) => {
+  app.get('/static/notifications', async (req, res) => {
+    const data = await DB.$notifications.find({}, { sort: { timestamp: -1 }, limit: 10 });
+    const retData = await data.toArray();
+    res.json(retData);
+  });
+}
