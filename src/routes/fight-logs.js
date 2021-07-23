ed56729c784222a7435ab5e3a2492bb1e63beeb5
rhisis <edeath1208@gmail.com>
2021-07-23 09:38:30 +0800

    pls lint

--- src/routes/fight-logs.js
+++ src/routes/fight-logs.js
@@ -1,24 +1,24 @@
-const { DB } = require("../db");
+const { DB } = require('../db');
 
 exports.route = (app) => {
-  app.get("/static/fights/:accountAddress", async (req, res) => {
+  app.get('/static/fights/:accountAddress', async (req, res) => {
     const { accountAddress } = req.params;
     if (!accountAddress) {
       return res
         .status(400)
-        .json({ error: "Invalid query. Must pass account address." });
+        .json({ error: 'Invalid query. Must pass account address.' });
     }
 
+    let fights;
+
     try {
-      const fights = await DB.$fights.find({ accountAddress });
-      res.json(
-        fights.map((fight) => {
-          delete fight.hash;
-          return fight;
-        })
-      );
+      fights = await DB.$fights.find({ accountAddress });
     } catch (error) {
       return res.status(500).json({ error });
     }
+
+    return res.json(
+      fights.map((fight) => fight),
+    );
   });
 };
