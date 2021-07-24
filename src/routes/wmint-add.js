646f4c47ae3c3da9998092d497ca5180833fafe9
Kyle J. Kemp <kyle@seiyria.com>
2021-07-24 17:18:11 -0500

    Update wmint-add.js

--- src/routes/wmint-add.js
+++ src/routes/wmint-add.js
@@ -3,7 +3,7 @@ const { DB } = require('../db');
 exports.route = (app) => {
   app.get('/leaderboard/weapon/mint/add', async (req, res) => {
     const {
-      hash, accountAddress, weaponId, gas, stars
+      hash, accountAddress, weaponId, gas, stars, 
     } = req.query;
     if (!hash || !accountAddress || !weaponId || !stars) {
       return res.status(400).json({ error: 'Invalid query. Must pass hash, accountAddress, weaponId, stars.' });
@@ -11,7 +11,7 @@ exports.route = (app) => {
 
     try {
       await DB.$wmints.replaceOne({ hash }, {
-        hash, accountAddress, weaponId, gas, stars
+        hash, accountAddress, weaponId, gas, stars,
       }, { upsert: true });
     } catch (error) {
       return res.status(500).json({ error });
