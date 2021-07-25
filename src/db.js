65fa8bb3dbfad1b86cb7741d1a5d1cce82f5e98a
seiyria <kyle@seiyria.com>
2021-07-25 14:17:19 -0500

    add more indexes

--- src/db.js
+++ src/db.js
@@ -1,13 +1,13 @@
 const { MongoClient } = require('mongodb');
 
 const DB_URI = process.env.MONGODB_URI;
-if(!DB_URI) {
+if (!DB_URI) {
   console.error('No env.MONGODB_URI set. Set one.');
   process.exit(0);
 }
 
-const API_SECRET = process.env.API_SECRET;
-if(!API_SECRET) {
+const { API_SECRET } = process.env;
+if (!API_SECRET) {
   console.log('Starting with no secret, be careful.');
 }
 
@@ -16,11 +16,10 @@ class Database {
     this.isReady = new Promise((resolve, reject) => {
       const client = new MongoClient(process.env.MONGODB_URI, { useUnifiedTopology: true });
       client.connect().then(() => {
+        console.log(`Connected to ${DB_URI}`);
 
-        console.log('Connected to ' + DB_URI);
-  
         const db = client.db('cryptoblades');
-        
+
         this.$log = db.collection('log');
 
         this.$leaderboard = db.collection('leaderboard');
@@ -39,10 +38,20 @@ class Database {
         this.$marketchanges = db.collection('leaderboard-marketchanges');
 
         this.$marketCharacters = db.collection('marketboard-characters');
+        this.$marketCharacters.createIndex({ timestamp: 1 });
         this.$marketCharacters.createIndex({ charId: 1 });
+        this.$marketCharacters.createIndex({ sellerAddress: 1 });
+        this.$marketCharacters.createIndex({
+          charLevel: 1, charElement: 1, price: 1, buyerAddress: 1,
+        });
 
         this.$marketWeapons = db.collection('marketboard-weapons');
+        this.$marketWeapons.createIndex({ timestamp: 1 });
         this.$marketWeapons.createIndex({ weaponId: 1 });
+        this.$marketWeapons.createIndex({ sellerAddress: 1 });
+        this.$marketWeapons.createIndex({
+          weaponElement: 1, weaponStars: 1, price: 1, buyerAddress: 1,
+        });
 
         this.$marketSales = db.collection('marketboard-sales');
         this.$marketSales.createIndex({ sellerAddress: 1 });
@@ -50,9 +59,10 @@ class Database {
         this.$dataPoints = db.collection('datapoints');
 
         this.$notifications = db.collection('notifications');
+        this.$notifications.createIndex({ timestamp: 1 });
 
-        Object.keys(this).forEach(key => {
-          if(!key.includes('leaderboard-')) return;
+        Object.keys(this).forEach((key) => {
+          if (!key.includes('leaderboard-')) return;
 
           this[key].createIndex({ hash: 1 }, { unique: true });
         });
