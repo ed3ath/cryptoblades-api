97f3698feee5fefe944890f34e905f78b07e1cab
seiyria <kyle@seiyria.com>
2021-07-25 15:46:35 -0500

    change redis client

--- src/helpers/redis-helper.js
+++ src/helpers/redis-helper.js
@@ -1,4 +1,4 @@
-const { createClient } = require('redis');
+const Redis = require('ioredis');
 
 let redis;
 const CACHE_URL = process.env.REDIS_URL;
@@ -6,9 +6,7 @@ const CACHE_URL = process.env.REDIS_URL;
 const init = async () => {
   if (!CACHE_URL) return;
   console.log(`Connecting to Redis @ ${CACHE_URL}`);
-  redis = createClient(CACHE_URL);
-  redis.on('error', (err) => console.log('Redis Client Error', err));
-  await redis.connect();
+  redis = new Redis(CACHE_URL);
 };
 
 module.exports.startRedis = init;
