380ce90e0c7987882bc4f410d69a883791b38423
seiyria <kyle@seiyria.com>
2021-07-25 15:29:51 -0500

    mimic db style

--- src/helpers/redis-helper.js
+++ src/helpers/redis-helper.js
@@ -1,11 +1,12 @@
 const { createClient } = require('redis');
 
 let redis;
+const CACHE_URL = process.env.REDIS_URL;
 
 const init = async () => {
-  if (!process.env.REDIS_URL) return;
-  console.log(`Connecting to Redis @ ${process.env.REDIS_URL}`);
-  redis = createClient(process.env.REDIS_URL);
+  if (!CACHE_URL) return;
+  console.log(`Connecting to Redis @ ${CACHE_URL}`);
+  redis = createClient(CACHE_URL);
   redis.on('error', (err) => console.log('Redis Client Error', err));
   await redis.connect();
 };
